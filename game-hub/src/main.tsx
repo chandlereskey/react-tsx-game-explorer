7af155ddb5632a66be0a7258f3dc444c216d201b
chandlereskey <chaneskey@gmail.com>
2025-07-09 23:06:33 -0400

    implemented color toggle in a slightly different way than mosh based on Chakra documentation

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -3,11 +3,15 @@ import { Provider } from "./components/ui/provider";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ColorModeProvider } from "./components/ui/color-mode";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
