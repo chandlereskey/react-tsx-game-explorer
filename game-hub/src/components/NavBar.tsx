8f8334730925ab99e9dd1b2a6e23eace9892712c
chandlereskey <chaneskey@gmail.com>
2025-07-16 00:13:37 -0400

    implement search

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeButton } from "./ui/color-mode";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between">
+    <HStack>
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeButton />
     </HStack>
   );
